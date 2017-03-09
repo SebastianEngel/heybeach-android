26d682fd517e1b8faa5dbcc7fabad87543d5dc06
Sebastian Engel <engel.sebastian@gmail.com>
2017-03-09 14:18:20 +0100

    Downloading images into the image list.

--- app/src/main/java/com/bitbucket/heybeach/ui/ImageListActivity.java
+++ app/src/main/java/com/bitbucket/heybeach/ui/ImageListActivity.java
@@ -2,29 +2,32 @@ package com.bitbucket.heybeach.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.ImageView;
-import com.bitbucket.heybeach.BuildConfig;
 import com.bitbucket.heybeach.DependencyProvider;
 import com.bitbucket.heybeach.R;
 import com.bitbucket.heybeach.domain.Image;
-import com.bitbucket.heybeach.ui.imageloading.ImageLoader;
 import java.util.List;
 
 public class ImageListActivity extends AppCompatActivity implements ImageListPresenter.ImageListView {
 
   private static final String LOG_TAG = ImageListActivity.class.getName();
 
-  ImageView imageView;
+  RecyclerView imageList;
 
   private ImageListPresenter imageListPresenter;
+  private ImageListAdapter imageListAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_image_list);
 
-    imageView = (ImageView) findViewById(R.id.image);
+    imageList = (RecyclerView) findViewById(R.id.image_list);
+    setupSignalsList();
+
     imageListPresenter = new ImageListPresenter(DependencyProvider.provideListImagesUseCase());
   }
 
@@ -43,10 +46,21 @@ public class ImageListActivity extends AppCompatActivity implements ImageListPre
   @Override
   public void updateImages(List<Image> images) {
     Log.i(LOG_TAG, "Updating list with images: " + images);
+    imageListAdapter.updateItems(images);
+  }
 
-    ImageLoader imageLoader = ImageLoader.getInstance();
-    imageLoader.load(BuildConfig.API_BASE_URL + "/" + images.get(0).getRelativeUrl(), imageView);
-    imageLoader.load(BuildConfig.API_BASE_URL + "/" + images.get(1).getRelativeUrl(), imageView);
+  private void setupSignalsList() {
+    imageList.setHasFixedSize(true);
+
+    imageListAdapter = new ImageListAdapter();
+    imageList.setAdapter(imageListAdapter);
+
+    LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+    layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+    imageList.setLayoutManager(layoutManager);
+
+    DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, layoutManager.getOrientation());
+    imageList.addItemDecoration(dividerItemDecoration);
   }
 
 }
