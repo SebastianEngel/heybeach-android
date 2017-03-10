8af19cc5e4441a0d45ef455bbbfe946efaa920fe
Sebastian Engel <engel.sebastian@gmail.com>
2017-03-10 20:00:10 +0100

    Removing logging statements.

--- app/src/main/java/com/bitbucket/heybeach/ui/LoginPresenter.java
+++ app/src/main/java/com/bitbucket/heybeach/ui/LoginPresenter.java
@@ -1,12 +1,9 @@
 package com.bitbucket.heybeach.ui;
 
-import android.util.Log;
 import com.bitbucket.heybeach.domain.LoginUseCase;
 import com.bitbucket.heybeach.domain.UseCaseException;
 
 class LoginPresenter extends MvpPresenter<LoginPresenter.LoginView> {
-
-  private static final String LOG_TAG = LoginPresenter.class.getName();
 
   private final LoginUseCase loginUseCase;
   private final ScreenNavigator screenNavigator;
@@ -22,7 +19,6 @@ class LoginPresenter extends MvpPresenter<LoginPresenter.LoginView> {
         loginUseCase.login(email, password);
         mainHandler.post(screenNavigator::navigateToAccountScreen);
       } catch (UseCaseException e) {
-        Log.e(LOG_TAG, "Failed to login user.", e);
         mainHandler.post(() -> view.showFailureMessage());
       }
     });
