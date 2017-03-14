f12bfbb655c8258b37984ce38ee26bead2aa7f70
Sebastian Engel <engel.sebastian@gmail.com>
2017-03-14 14:39:23 +0100

    Implementing a memory image cache.

--- app/src/main/java/com/bitbucket/heybeach/ui/imageloading/ImageLoader.java
+++ app/src/main/java/com/bitbucket/heybeach/ui/imageloading/ImageLoader.java
@@ -1,6 +1,7 @@
 package com.bitbucket.heybeach.ui.imageloading;
 
 import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
 import android.util.Log;
 import android.widget.ImageView;
 import java.net.MalformedURLException;
@@ -15,6 +16,7 @@ public class ImageLoader {
 
   private final ExecutorService executorService = Executors.newCachedThreadPool();
   private final Map<ImageView, ImageDownload> viewToDownloads = new WeakHashMap<>();
+  private final ImageCache imageCache = new ImageCache();
 
   private static ImageLoader instance;
 
@@ -28,25 +30,56 @@ public class ImageLoader {
   }
 
   public void load(String imageUrl, ImageView imageView, Callback callback) {
-    // If there is already an image download for this ImageView, remove it.
-    ImageDownload imageDownload = viewToDownloads.remove(imageView);
-    if (imageDownload != null) {
-      imageDownload.cancel();
-    }
+    Bitmap bitmap = imageCache.get(imageUrl);
+    if (bitmap != null) {
+      Log.d(LOG_TAG, "Taking image from cache for URL =  " + imageUrl);
+      callback.onDownloadCompleted(bitmap);
+    } else {
 
-    try {
-      imageDownload = new ImageDownload(imageUrl, callback);
-      viewToDownloads.put(imageView, imageDownload);
-      executorService.submit(imageDownload);
-    } catch (MalformedURLException e) {
-      Log.w(LOG_TAG, "Unable to download image due to invalid image url.", e);
+      // If there is already an image download for this ImageView, remove it.
+      ImageDownload imageDownload = viewToDownloads.remove(imageView);
+      if (imageDownload != null) {
+        imageDownload.cancel();
+      }
+
+      try {
+        imageDownload = new ImageDownload(imageUrl, new Callback() {
+          @Override public void onDownloadCompleted(Bitmap bitmap) {
+            synchronized (imageCache) {
+              imageCache.put(imageUrl, bitmap);
+            }
+            callback.onDownloadCompleted(bitmap);
+          }
+
+          @Override public void onDownloadFailed() {
+            callback.onDownloadFailed();
+          }
+        });
+        viewToDownloads.put(imageView, imageDownload);
+        executorService.submit(imageDownload);
+      } catch (MalformedURLException e) {
+        Log.w(LOG_TAG, "Unable to download image due to invalid image url.", e);
+      }
     }
   }
-
 
   public interface Callback {
     void onDownloadCompleted(Bitmap bitmap);
     void onDownloadFailed();
+  }
+
+  static class ImageCache extends LruCache<String, Bitmap> {
+
+    private static final int CACHE_SIZE_MB = 50 * 1024 * 1024; // 50 MB
+
+    ImageCache() {
+      super(CACHE_SIZE_MB);
+    }
+
+    @Override
+    protected int sizeOf(String key, Bitmap bitmap) {
+      return bitmap.getByteCount();
+    }
   }
 
 }
